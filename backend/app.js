36246cc994853477babbb274b46ecc58f6b268d9
datle-dev <dle.dev@outlook.com>
2024-06-03 09:22:14 -0400

    Add passport session middleware to app

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv/config';
 import mongoose from 'mongoose';
+import session from 'express-session';
+import MongoStore from 'connect_mongo';
 import path from 'path';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+import './auth/passport.js';
 
 import indexRouter from './routes/index.js';
 import signupRouter from './routes/signup.js';
@@ -30,6 +34,31 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionStore = MongoStore.create({
+  client: mongoose.connection.getClient(),
+  collectionName: 'sessions',
+});
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 day in ms
+    },
+  }),
+);
+
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  console.log(req.session);
+  console.log(req.user);
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/signup', signupRouter);
