9ee78914c8d31e905b074781c11179407ded31c0
datle-dev <dle.dev@outlook.com>
2024-06-03 08:01:22 -0400

    Configure connection to mongodb via mongoose
    Use mongoose to set up connection to mongodb database. Use dotenv to
    store mongo connection URI in .env file and reference in app.js when
    making connection to the database.

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,29 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+dotenv.config();
+
 import indexRouter from './routes/index.js';
 
 const app = express();
+
+// connect to database
+mongoose.set('strictQuery', false);
+const mongoDB = process.env.MONGO_URI;
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(mongoDB);
+  console.log(
+    `mongoose connection readyState = ${mongoose.connection.readyState}`,
+  );
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
