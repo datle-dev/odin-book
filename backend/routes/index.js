3a5201ee989925c29950371c31cc0a3efdf26bac
datle-dev <dle.dev@outlook.com>
2024-06-03 07:47:23 -0400

    Update index route to use express-async-handler

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.json({
-    title: 'index',
-  });
-});
+router.get(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    res.json({
+      title: 'index',
+    });
+  }),
+);
 
 export default router;
