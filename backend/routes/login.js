d25e240bc12033e1ae610063c8662b4fb8695bf5
datle-dev <dle.dev@outlook.com>
2024-06-03 07:48:01 -0400

    Initial commit of signup and login routes

---
+++ backend/routes/login.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+
+const router = express.Router();
+
+router.get(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    res.json({
+      title: 'login',
+    });
+  }),
+);
+
+export default router;
