1a98288743aeb526b59dadbb6e342047a0580a08
datle-dev <dle.dev@outlook.com>
2024-06-05 07:47:36 -0400

    Add cookie-parser package and include in backend app

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ import session from 'express-session';
 import MongoStore from 'connect-mongo';
 import path from 'path';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -35,6 +36,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
 const sessionStore = MongoStore.create({
   client: mongoose.connection.getClient(),
