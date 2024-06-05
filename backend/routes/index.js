bac26acaabb0d30de89d36f9588d41f03f1c9310
datle-dev <dle.dev@outlook.com>
2024-06-05 07:49:36 -0400

    Create authenticated test and logout routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
+import { isAuth } from '../auth/auth-utils.js';
 
 const router = express.Router();
 
@@ -8,6 +9,28 @@ router.get(
   asyncHandler(async (req, res, next) => {
     res.json({
       title: 'index',
+    });
+  }),
+);
+
+router.get(
+  '/test',
+  isAuth,
+  asyncHandler(async (req, res, next) => {
+    res.json({
+      title: 'test successful',
+    });
+  }),
+);
+
+router.post(
+  '/logout',
+  asyncHandler(async (req, res, next) => {
+    req.logout((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/');
     });
   }),
 );
