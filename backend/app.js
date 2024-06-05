b1d7557b1094dc7866b2f33ebca1b59433675c3f
datle-dev <dle.dev@outlook.com>
2024-06-05 07:48:06 -0400

    Configure CORS with origin and credentials

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,12 @@ async function main() {
   );
 }
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+};
+
+app.use('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
